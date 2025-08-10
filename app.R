c25aca1831014f75c2c202852dfc20899dc40cd2
ternes90 <142872309+ternes90@users.noreply.github.com>
2025-08-10 22:25:36 +0200

    update

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(ggbeeswarm)
 library(readxl)
 library(DT)
 library(scales)
-#library(httr2)
+library(httr2)
 library(jsonlite)
 
 last_update <- tryCatch(readLines("data/last_updated.txt", warn = FALSE), error = function(e) "unbekannt")
