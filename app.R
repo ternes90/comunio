51b3ce5580111091806e0ac56aa4b62897fc7aa7
Dr. Ternes <ternes90@gmail.com>
2025-09-20 10:10:42 +0200

    up

--- app.R
+++ app.R
@@ -494,6 +494,15 @@ ui <- navbarPage(
 # ---- SERVER ----
 server <- function(input, output, session) {
   
+  #Install ggrepel once
+  observeEvent(input$show_labels, {
+    req(isTRUE(input$show_labels))
+    if (!requireNamespace("ggrepel", quietly = TRUE)) {
+      install.packages("ggrepel", repos = "https://cran.rstudio.com")
+    }
+    library(ggrepel)
+  }, once = TRUE)
+  
   #Save beeswarm position
   pos_qr <- ggbeeswarm::position_quasirandom(width = 0.22)
   #Update stempel
@@ -3060,11 +3069,6 @@ server <- function(input, output, session) {
     p <- ggplot(df, aes(x = "Vortag", y = rel_Vortag)) +
       geom_boxplot(width = 0.2, outlier.shape = NA,
                    fill = "#ff6b6b", color = "#ff6b6b", alpha = 0.25)
-    
-    if (isTRUE(input$show_labels)) {
-      install.packages("ggrepel", repos="https://cran.rstudio.com")
-    library(ggrepel)
-    }
     
     if (input$color_by == "Trend") {
       p <- p + geom_point(aes(color = Trend), position = pos_qr, size = 3.5, alpha = 0.6,
